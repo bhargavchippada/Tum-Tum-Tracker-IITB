9bd94c0b14f74de74e7f07553a3281c821470efe
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-03-07 00:47:10 +0530

    basic xml layouts done

---
+++ TumTum/src/tumtumtracker/TrackingActivity.java
@@ -0,0 +1,15 @@
+package tumtumtracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.iitbombay.tumtumtracker.R;
+
+public class TrackingActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_tracking);
+	}
+}
